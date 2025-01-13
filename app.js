7b7cb055489c4d7be9e8a669d76ac5eee083bb4e
Pratyushanand26 <anandpratyush26@gmail.com>
2025-01-13 11:27:15 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+
+let body=document.querySelector("body");
+let h1=document.querySelector("h1");
+
+function red(){
+    
+let body=document.querySelector("body");
+    body.style.backgroundColor="red";
+}
+
+function yellow(){
+    body.style.backgroundColor="yellow";
+    h1.style.color="black";
+}
+function orange(){
+    body.style.backgroundColor="orange";
+}
+function black(){
+    body.style.backgroundColor="black";
+    h1.style.color="white";
+}
+function grey(){
+    body.style.backgroundColor="grey";
+}
+function purple(){
+    body.style.backgroundColor="purple";
+}
+
+function green(){
+    body.style.backgroundColor="green";
+}
